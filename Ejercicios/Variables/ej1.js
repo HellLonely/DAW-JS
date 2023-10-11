3600c48c40c83e73822c39c79235ffaf1e3ae0ed
HellLonely <helllonely404@gmail.com>
2023-10-11 10:15:42 +0200

    'Ejercicios'

---
+++ Ejercicios/Variables/ej1.js
@@ -0,0 +1,11 @@
+/**
+ * Ejercicio 1: Variables
+ *
+ * 1 - Crea una variable de tipo let y var y prueba a sumarlas, guarda el resultado
+ *      en otra variable let.
+ * 2 - Crea una variable de tipo const y prueba a restarla con una variable de tipo 
+ *       var, guarda el resultado en la variable const que ya creaste.
+ * 
+ * 3 - Que pasa en el apartado anterior?
+ * 
+ */
---
+++ Ejercicios/Variables/solucion-ej1.js
@@ -0,0 +1,26 @@
+/**
+ * Ejercicio 1: Variables
+ *
+ * 1 - Crea una variable de tipo let y var y prueba a sumarlas, guarda el resultado
+ *      en otra variable let.
+ * 2 - Crea una variable de tipo const y prueba a restarla con una variable de tipo 
+ *       var, guarda el resultado en la variable const que ya creaste.
+ * 
+ * 3 - Que pasa en el apartado anterior?
+ * 
+ */
+
+
+
+let num1 = 1;
+var num2 = 2;
+
+let res1 = num1 + num2;
+
+
+const variable1 = 5;
+var variable2 = 2;
+  
+variable1 = variable1 - variable2;
+
+// Da errro ya que no se puede volver a definir la constante.
